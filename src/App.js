5ee7ca4670d4dfbfad8943410f4e27ca0fa5c81e
Antrâxx <h.julien62@gmail.com>
2022-06-09 14:31:01 +0200

    Front div Burger avec polices

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ function App() {
   return (
     <div className="App">
 
-    <div className="container">
+    <div className="container d-flex flex-column justify-content-center align-items-center">
 
-      <div className="burger col-6 card border-dark rounded d-flex flex-row justify-content-around p-3 mx-auto">
+      <div className="burger col-6 card rounded d-flex flex-row justify-content-between p-3 mx-auto">
 
-      <div className="picture">
-        <img src="https://foodish-api.herokuapp.com/images/burger/burger42.jpg" className="rounded"/>
-      </div>
+      <div className="d-flex flex-row">
+        <div className="picture">
+          <img src="https://foodish-api.herokuapp.com/images/burger/burger42.jpg" className="rounded"/>
+        </div>
 
-      <div className="info">
-        <h2>Normal Burger</h2>
-        <p>Un bon burger</p>
-        <button className="btn btn-warning btn-outline-dark">Voir</button>
+        <div className="info d-flex flex-column justify-content-start p-2">
+          <h2>Normal Burger</h2>
+          <p>Un bon burger</p>
+          <button className="btn btn-warning text-light">Voir</button>
+        </div>
       </div>
 
       <div className="price d-flex justify-content-end align-items-center">
